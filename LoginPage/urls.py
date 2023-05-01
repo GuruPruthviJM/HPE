805f040951ed4924b7d2396e351fa359c7d9da46
GuruPruthviJM <105730764+GuruPruthviJM@users.noreply.github.com>
2023-05-01 22:02:24 +0530

    Update urls.py

--- LoginPage/urls.py
+++ LoginPage/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from app1 import views
@@ -29,3 +31,6 @@ urlpatterns = [
     path('submitedQuery/', views.submitedQuery, name='submitedQuery'),
     path('contact/', views.ContactPage.as_view(), name='contact'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

ae674068e41831b79e668b13fdf22841cd0a675b
GuruPruthviJM <105730764+GuruPruthviJM@users.noreply.github.com>
2023-05-02 21:18:59 +0530

    Update settings.py

--- LoginPage/settings.py
+++ LoginPage/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+s#wcz!tw2^j!#dzpu4o2hlc^rne*dag*6a4q6r1tpj1$)nk=p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
